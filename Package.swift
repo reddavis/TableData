a83e738bffceb9bc5b6fb7202e702ce25c652420
Red Davis <me@red.to>
2020-10-30 13:10:10 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TableData",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "TableDataTests",
             dependencies: ["TableData"]),
